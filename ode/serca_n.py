f34a7a36262e0b15e61b195850760f5bb4fa8b20
Kabir Dabholkar <kabir.dabholkar@students.iiserpune.ac.in>
2018-08-24 19:13:43 +0530

    changed everything to concentrations

--- ode/serca_n.py
+++ ode/serca_n.py
@@ -3,8 +3,9 @@ from matplotlib.pyplot import *
 import numpy as np
 
 cai = 100.0e-9
-cae = 1000.0e-6
+cae = 0
 nserca = 8678.0
+N_avo=6.022e23
 
 # Reaction Rates
 kx0_x1=2*1.0e8
@@ -43,25 +44,25 @@ ncae = cae*vol_er*6.022*1e8
 
 # SERCA ODE model
 def serca_ode(v ,t):
-	fx0, fx1, fx2,	fy0, fy1, fy2, ncae = v 
+	x0, x1, x2, y0, y1, y2, cae = v 
 	
-	dx0 = fx0*(-kx0_x1*ncai-kx0_y0)+fx1*kx1_x0+fy0*ky0_x0
-	dx1 = fx1*(-kx1_x2*ncai-kx1_x0)+fx0*ncai*kx0_x1+fx2*kx2_x1
-	dx2 = fx2*(-kx2_y2-kx2_x1)+fx1*ncai*kx1_x2+fy2*ky2_x2
+	dx0 = x0*(-kx0_x1*cai-kx0_y0)+x1*kx1_x0+y0*ky0_x0
+	dx1 = x1*(-kx1_x2*cai-kx1_x0)+x0*cai*kx0_x1+x2*kx2_x1
+	dx2 = x2*(-kx2_y2-kx2_x1)+x1*ncai*kx1_x2+y2*ky2_x2
 
-	dy0 = fy0*(-ky0_y1*ncae-ky0_x0)+fy1*ky1_y0+fx0*kx0_y0
-	dy1 = fy1*(-ky1_y2*ncae-ky1_y0)+fy0*ncae*ky0_y1+fy2*ky2_y1
-	dy2 = fy2*(-ky2_x2-ky2_y1)+fy1*ncae*ky1_y2+fx2*kx2_y2
+	dy0 = y0*(-ky0_y1*cae-ky0_x0)+y1*ky1_y0+x0*kx0_y0
+	dy1 = y1*(-ky1_y2*cae-ky1_y0)+y0*cae*ky0_y1+y2*ky2_y1
+	dy2 = y2*(-ky2_x2-ky2_y1)+y1*cae*ky1_y2+x2*kx2_y2
 	
-	dcae = -ncae*(fy1*ky1_y2 + fy0*ky0_y1) + (fy1*ky1_y0 + fy2*ky2_y1)
+	dcae = -cae*(y1*ky1_y2 + y0*ky0_y1) + (y1*ky1_y0 + y2*ky2_y1)
 
 	return [dx0, dx1, dx2, dy0, dy1, dy2, dcae]
 	
 # Initial Conditions
-v0 = [0.75554529, 0.18054556, 0.010757505, 0.015718635, 0.026363904, 0.011069105]
-v0 = [i*nserca for i in v0]
-v0.append(ncae)
-
+v0 = np.array([0.75554529, 0.18054556, 0.010757505, 0.015718635, 0.026363904, 0.011069105,1])
+v0 = v0*nserca/(vol_er/1e18)/N_avo
+v0[6]=cae
+print v0
 tstep = 10e-5
 tf = 1
 t = np.linspace(0, tf, tf/tstep+1)
@@ -69,11 +70,11 @@ t = np.linspace(0, tf, tf/tstep+1)
 # Solve ODE
 sol = odeint(serca_ode, v0, t)
 
-label = ['x0', 'x1', 'x2', 'y0', 'y1', 'y2', 'ncae']
-for i in range(6):
+label = ['x0', 'x1', 'x2', 'y0', 'y1', 'y2', 'cae']
+for i in range(7):
 	plot(t,sol[:,i], label=label[i])
 #show()
 #close()
-plot(t,sol[:,6]/23.45, label='Cae')
+#plot(t,sol[:,6]/23.45, label='Cae')
 legend()
 show()
