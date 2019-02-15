246889fd84d6cb4b61c04bd527d5eae830617f27
Kabir Dabholkar <kabir.dabholkar@students.iiserpune.ac.in>
2019-02-15 14:58:18 +0530

    refilling time

--- test291018.sh
+++
@@ -1,4 +0,0 @@
-seq 1|parallel mcell ryr_750_leak/RSnostim.mdl -seed {}
-#seq 8|parallel mcell ryr_500_leak/RSnostim.mdl -seed {}
-#seq 8|parallel mcell ryr_750/RSnostim.mdl -seed {}
-#seq 8|parallel mcell ryr_500/RSnostim.mdl -seed {}
