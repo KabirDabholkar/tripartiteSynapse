7db8b1036138236deaca7b567c7da7ef22c740b3
Kabir Dabholkar <kabir.dabholkar@students.iiserpune.ac.in>
2019-03-02 00:41:24 +0530

    using clamp conc

---
+++ brute500_8/run.sh
@@ -0,0 +1,30 @@
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak2500 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak2500.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak3750 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak3750.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak5000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak5000.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak6250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak6250.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak7500 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak7500.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak8750 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak8750.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak10000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak10000.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak11250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak11250.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak12500 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak12500.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N kleak13750 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/RSnostim_sm0_ic250_sf2_kleak13750.mdl' pbs.py
+
--- brute750_9/run.sh
+++ brute750_9/run.sh
@@ -1,33 +1,33 @@
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak60000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak60000.mdl' pbs.py
+qsub -N kleak60000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak60000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak61000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak61000.mdl' pbs.py
+qsub -N kleak61000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak61000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak62000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak62000.mdl' pbs.py
+qsub -N kleak62000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak62000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak63000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak63000.mdl' pbs.py
+qsub -N kleak63000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak63000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak64000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak64000.mdl' pbs.py
+qsub -N kleak64000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak64000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak65000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak65000.mdl' pbs.py
+qsub -N kleak65000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak65000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak66000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak66000.mdl' pbs.py
+qsub -N kleak66000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak66000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak67000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak67000.mdl' pbs.py
+qsub -N kleak67000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak67000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak68000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak68000.mdl' pbs.py
+qsub -N kleak68000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak68000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak69000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak69000.mdl' pbs.py
+qsub -N kleak69000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak69000.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N sm0_ic275_sf2_kleak70000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak70000.mdl' pbs.py
+qsub -N kleak70000 -v I='/home/subhadra/kabir/tripartiteSynapse/brute750_9/RSnostim_sm0_ic275_sf2_kleak70000.mdl' pbs.py
 
