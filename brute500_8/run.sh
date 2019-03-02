d58bd87e48833df35181b3a578a8f9c794a61964
Kabir Dabholkar <kabir.dabholkar@students.iiserpune.ac.in>
2019-03-02 21:12:55 +0530

    clamp leak and sm slopes working

--- brute500_8/run.sh
+++ brute500_8/run.sh
@@ -1,15 +1,27 @@
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N er_clamp_sm0_ic260 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm0_ic260_sf2_kleak0.mdl' pbs.py
+qsub -N sm30_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm30_ic250_sf2_kleak0.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N er_clamp_sm0_ic263 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm0_ic263_sf2_kleak0.mdl' pbs.py
+qsub -N sm60_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm60_ic250_sf2_kleak0.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N er_clamp_sm0_ic266 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm0_ic266_sf2_kleak0.mdl' pbs.py
+qsub -N sm90_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm90_ic250_sf2_kleak0.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N er_clamp_sm0_ic269 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm0_ic269_sf2_kleak0.mdl' pbs.py
+qsub -N sm120_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm120_ic250_sf2_kleak0.mdl' pbs.py
 
 sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
-qsub -N er_clamp_sm0_ic272 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm0_ic272_sf2_kleak0.mdl' pbs.py
+qsub -N sm150_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm150_ic250_sf2_kleak0.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N sm180_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm180_ic250_sf2_kleak0.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N sm210_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm210_ic250_sf2_kleak0.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N sm240_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm240_ic250_sf2_kleak0.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-20/' pbs.py
+qsub -N sm270_ic250 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_8/er_clamp_sm270_ic250_sf2_kleak0.mdl' pbs.py
 
