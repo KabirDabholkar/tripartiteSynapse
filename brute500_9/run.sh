2bccdc5e460e0adcbf4697e0287eec9cb18a5e96
Kabir Dabholkar <kabir.dabholkar@students.iiserpune.ac.in>
2019-03-06 15:49:44 +0530

    sm kleak relationship not perfect

---
+++ brute500_9/run.sh
@@ -0,0 +1,27 @@
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s30i250f2l1263 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s30i250f2l1263.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s60i250f2l1770 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s60i250f2l1770.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s90i250f2l2067 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s90i250f2l2067.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s120i250f2l2277 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s120i250f2l2277.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s150i250f2l2441 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s150i250f2l2441.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s180i250f2l2574 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s180i250f2l2574.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s210i250f2l2687 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s210i250f2l2687.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s240i250f2l2784 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s240i250f2l2784.mdl' pbs.py
+
+sed --i 's/^#PBS -J.*/#PBS -J 1-40/' pbs.py
+qsub -N s270i250f2l2871 -v I='/home/subhadra/kabir/tripartiteSynapse/brute500_9/RSnostim_s270i250f2l2871.mdl' pbs.py
+
