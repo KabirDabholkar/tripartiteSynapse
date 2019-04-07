5cf7ed9e9db8c5f38c9409b3e4a83ec6b8e9f45e
Kabir Dabholkar <kabir.dabholkar@students.iiserpune.ac.in>
2019-04-07 22:46:20 +0530

    fixed ryr problem

---
+++ tools/sync.py
@@ -0,0 +1,6 @@
+#sync data back to this system
+import sys
+import subprocess
+
+outfolder=sys.argv[1]
+p=subprocess.call(["rsync","-arhP","subhadra@192.168.1.244:/storage/subhadra/kabir/output/ppf/"+outfolder,"/data/kabir/output/ppf/"])
---
+++ tools/sync.sh
@@ -0,0 +1,2 @@
+seq 20 20 200 | parallel rsync -a subhadra@192.168.1.244:/storage/subhadra/kabir/output/ppf/R150control/RSI{}V90 /data/kabir/output/ppf/R150control/
+
